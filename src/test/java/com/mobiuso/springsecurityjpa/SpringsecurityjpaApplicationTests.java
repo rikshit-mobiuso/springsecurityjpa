c1f88e664b5b23de2955cbd0b1b26dd0505809f0
Rikshit Soni <97026301+rikshit-mobiuso@users.noreply.github.com>
2022-03-11 14:44:00 +0530

    initial commit

---
+++ src/test/java/com/mobiuso/springsecurityjpa/SpringsecurityjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobiuso.springsecurityjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityjpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
